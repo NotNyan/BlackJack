087494f83b77d9a7e4b9e9fc8e154ad6795eb611
NotNyan <nyanvisionsz@yahoo.com>
2015-05-29 13:25:24 -0500

    Finished the game!

--- SchoolProjects/src/com/game/blackjack.java
+++ SchoolProjects/src/com/game/blackjack.java
@@ -4,6 +4,11 @@ public class blackjack {
 	static Scanner input = new Scanner(System.in);
 	static boolean question = false;
 	
+	static boolean win = false;
+	static boolean lose = false;
+	static boolean busted = false;
+	static int player_last_number = 0;
+	static int dealer_last_number = 0;
 	static Random random = new Random();
 	static boolean testMode = true;
     static int rndm;
@@ -130,11 +135,13 @@ public class blackjack {
   				 playerDeck[1] = cardValue[12];
  				 playerDeckName[1] = cards[12];
  				 break;
-  		 }
+  		 } 
+  		 player_last_number = 1;
+  		playerTotal = playerDeck[0] + playerDeck[1];
  		//If Your first and second were both aces it would make your total 12.
- 		if ((playerDeck[0] == 11) && (playerDeck[1] == 11)){playerTotal = 12; checkDeck();}
- 		playerTotal = playerDeck[0] + playerDeck[1];
-  		 checkDeck();
+ 		if ((playerDeck[0] == 11) && (playerDeck[1] == 11)){playerTotal = 12;}
+ 		if (playerTotal == 21){System.out.println("You win!"); win = true;}
+
   		 
   		rndm = new Random().nextInt(13)+1;	
   		 switch (rndm){
@@ -254,19 +261,158 @@ public class blackjack {
 	}
     
 	public static void checkDeck(){ 
-		if (playerTotal == 21){System.out.println("You win!");}
+		if (playerTotal == dealerTotal){System.out.println("You guys tied. The dealer wins!"); lose = true;}
+	    else if (playerTotal == 21){System.out.println("You win!"); win = true;}
+	    else if (playerTotal >= 22){System.out.println("You Busted..."); busted = true;}
+	    else if (playerTotal > dealerTotal){System.out.println("You scored higher than the dealer!, You win!"); win = true;}	    
+	    else if (dealerTotal > 21){System.out.println("Dealer busted. You win!"); win = true;}
+	    else if (dealerTotal > playerTotal){System.out.println("Dealer scored closer than you!,You lose!"); lose = true;}    	
 		else{System.out.println("Your total is total is: "+playerTotal);}
 		
 	}
     
 	public static void hit(){
-   	 System.out.println("I work.");
    	 question = false;
+   	 player_last_number++;
+ 	 rndm = new Random().nextInt(13)+1;		 
+		 switch (rndm){
+			 case 1:
+				 playerDeck[player_last_number] = cardValue[0];
+				 playerDeckName[player_last_number] = cards[0];
+				 break;
+			 case 2:
+				 playerDeck[player_last_number] = cardValue[1];
+				 playerDeckName[player_last_number] = cards[1];
+				 break;
+			 case 3:
+				 playerDeck[player_last_number] = cardValue[2];
+				 playerDeckName[player_last_number] = cards[2];
+				 break;
+			 case 4:
+				 playerDeck[player_last_number] = cardValue[3];
+				 playerDeckName[player_last_number] = cards[3];
+				 break;
+			 case 5:
+				 playerDeck[player_last_number] = cardValue[4];
+				 playerDeckName[player_last_number] = cards[4];
+				 break;
+			 case 6:
+				 playerDeck[player_last_number] = cardValue[5];
+				 playerDeckName[player_last_number] = cards[5];
+				 break;
+			 case 7:
+				 playerDeck[player_last_number] = cardValue[6];
+				 playerDeckName[player_last_number] = cards[6];
+				 break;
+			 case 8:
+				 playerDeck[player_last_number] = cardValue[7];
+				 playerDeckName[player_last_number] = cards[7];
+				 break;
+			 case 9:
+				 playerDeck[player_last_number] = cardValue[8];
+				 playerDeckName[player_last_number] = cards[8];
+				 break;
+			 case 10:
+				 playerDeck[player_last_number] = cardValue[9];
+				 playerDeckName[player_last_number] = cards[9];
+				 break;
+			 case 11:
+				 playerDeck[player_last_number] = cardValue[10];
+				 playerDeckName[player_last_number] = cards[10];
+				 break;
+			 case 12:
+				 playerDeck[player_last_number] = cardValue[11];
+				 playerDeckName[player_last_number] = cards[11];
+				 break;
+			 case 13:
+				 playerDeck[player_last_number] = cardValue[12];
+				 playerDeckName[player_last_number] = cards[12];
+				 break;
+		 }
+		 playerTotal += playerDeck[player_last_number];
+		 question = true;
+		 if (playerTotal >= 22){busted = true; question = false; System.out.println("You busted!");}
+		 System.out.println("You have a total of "+playerTotal);
 	}
     
 	public static void stand(){
-   	 
-	}
+   	 	do {
+   	 		dealer_last_number++;
+   	 		//about to draw :-)
+   	  		rndm = new Random().nextInt(13)+1;	
+     		 switch (rndm){
+     			 case 1:
+     				 dealerDeck[dealer_last_number] = cardValue[0];
+     				 dealerDeckName[dealer_last_number] = cards[0];
+     				 break;
+     			 case 2:
+     				 dealerDeck[dealer_last_number] = cardValue[1];
+     				 dealerDeckName[dealer_last_number] = cards[1];
+     				 break;
+     			 case 3:
+     				 dealerDeck[dealer_last_number] = cardValue[2];
+     				 dealerDeckName[dealer_last_number] = cards[2];
+     				 break;
+     			 case 4:
+     				 dealerDeck[dealer_last_number] = cardValue[3];
+     				 dealerDeckName[dealer_last_number] = cards[3];
+     				 break;
+     			 case 5:
+     				 dealerDeck[dealer_last_number] = cardValue[4];
+     				 dealerDeckName[dealer_last_number] = cards[4];
+     				 break;
+     			 case 6:
+     				 dealerDeck[dealer_last_number] = cardValue[5];
+    				 dealerDeckName[dealer_last_number] = cards[5];
+     				 break;
+     			 case 7:
+     				 dealerDeck[dealer_last_number] = cardValue[6];
+    				 dealerDeckName[dealer_last_number] = cards[6];
+     				 break;
+     			 case 8:
+     				 dealerDeck[dealer_last_number] = cardValue[7];
+    				 dealerDeckName[dealer_last_number] = cards[7];
+     				 break;
+     			 case 9:
+     				 dealerDeck[dealer_last_number] = cardValue[8];
+    				 dealerDeckName[dealer_last_number] = cards[8];
+     				 break;
+     			 case 10:
+     				 dealerDeck[dealer_last_number] = cardValue[9];
+    				 dealerDeckName[dealer_last_number] = cards[9];
+     				 break;
+     			 case 11:
+     				 dealerDeck[dealer_last_number] = cardValue[10];
+    				 dealerDeckName[dealer_last_number] = cards[10];
+     				 break;
+     			 case 12:
+     				 dealerDeck[dealer_last_number] = cardValue[11];
+    				 dealerDeckName[dealer_last_number] = cards[11];
+     				 break;
+     			 case 13:
+     				 dealerDeck[dealer_last_number] = cardValue[12];
+    				 dealerDeckName[dealer_last_number] = cards[12];
+    				 break;
+     		 }	
+     		 dealerTotal += dealerDeck[dealer_last_number];
+   	 	}while (dealerTotal <= 16);
+   	 	checkDeck();
+   	 	if (lose || win){question = false;}
+   	 	
+   	 		
+   	 	}
+
+//	public static void display(){
+//    	System.out.println(playerDeckName[0] +" "+ playerDeck[0]);
+//    	System.out.println(playerDeckName[1] +" "+ playerDeck[1]);
+//    	System.out.println(playerDeckName[2] +" "+ playerDeck[2]);
+//    	System.out.println(playerDeckName[3] +" "+ playerDeck[3]);
+//    	System.out.println(dealerDeckName[0] +" "+ dealerDeck[0]);
+//    	System.out.println(dealerDeckName[1] +" "+ dealerDeck[1]);
+//    	System.out.println(dealerDeckName[2] +" "+ dealerDeck[2]);
+//    	System.out.println(dealerDeckName[3] +" "+ dealerDeck[3]);
+//	}
+
     
 	public static void main(String[] args){
 		String response;
